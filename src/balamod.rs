ce297e8032afa493b2c2d0a57d6e77a4f2424c8e
UwU <mmimi0593@gmail.com>
2023-12-16 02:38:03 +0100

    Good injector and decompiler/deobf

--- src/balamod.rs
+++ src/balamod.rs
@@ -2,9 +2,10 @@ use std::path::{Path, PathBuf};
 use std::fs::File;
 use std::fs;
 use zip::ZipArchive;
-use std::io::{BufReader, Write, Read, Cursor, BufRead};
-use colour::{blue, cyan, green_ln, red_ln, yellow_ln};
+use std::io::{BufReader, Write, Read, Cursor};
+use colour::red_ln;
 use zip::{ZipWriter, CompressionMethod, write::FileOptions};
+use libflate::deflate::Encoder;
 
 #[derive(Clone)]
 pub struct Balatro {
@@ -19,12 +20,7 @@ impl Balatro {
         replace_file_in_exe(exe_path, file_name, new_contents)
     }
 
-    pub fn extract_ressource_pack(&self, output_folder: &str, textures: bool, shaders: bool, sounds: bool, fonts: bool) -> Result<(), std::io::Error> {
-        if PathBuf::from(output_folder).exists() {
-            yellow_ln!("{} already exists, deleting it...", output_folder);
-            fs::remove_dir_all(output_folder)?;
-        }
-
+    pub fn get_file_data(&self, file_name: &str) -> Result<Vec<u8>, std::io::Error>{
         let exe_path_buf = self.path.join("Balatro.exe");
         let exe_path = exe_path_buf.to_str().expect("Failed to convert exe_path to str");
         let file = File::open(exe_path)?;
@@ -32,132 +28,14 @@ impl Balatro {
 
         for i in 0..archive.len() {
             let mut file = archive.by_index(i)?;
-            let file_name = file.name().to_string();
-            if file_name.ends_with(".DS_Store") {
-                continue;
-            }
-
-            if textures && file_name.starts_with("resources/textures/") ||
-                shaders && file_name.starts_with("resources/shaders/") ||
-                sounds && file_name.starts_with("resources/sounds/") ||
-                fonts && file_name.starts_with("resources/fonts/")
-            {
+            if file.name() == file_name {
                 let mut contents = Vec::new();
                 file.read_to_end(&mut contents)?;
-                let mut path = PathBuf::from(output_folder);
-                path.push(file_name.strip_prefix("resources/").unwrap());
-
-                if file_name.ends_with('/') {
-                    fs::create_dir_all(&path)?;
-                } else {
-                    if let Some(parent) = path.parent() {
-                        fs::create_dir_all(parent)?;
-                    }
-                    fs::write(path, contents)?;
-                }
+                return Ok(contents);
             }
         }
-
-        if textures {
-            let resizes = vec![1, 2, 4, 8];
-            let mut resizes_textures_path = Vec::new();
-            for resize in &resizes {
-                let mut path = PathBuf::from(output_folder);
-                path.push(format!("textures/{}x", resize));
-                resizes_textures_path.push(path);
-            }
-
-            let mut delete_textures_path = Vec::new();
-            for _ in resizes {
-                delete_textures_path.push(Vec::new());
-            }
-
-            let mut index = 0;
-            for path in &resizes_textures_path {
-                let files = fs::read_dir(&path)?;
-
-                for file in files {
-                    let file = file?;
-                    let file_name = file.file_name();
-                    let file_name = file_name.to_str().unwrap();
-
-                    for i in index+1..resizes_textures_path.len() {
-                        let mut path = resizes_textures_path[i].clone();
-                        path.push(file_name);
-
-                        if path.exists() {
-                            fs::remove_file(path.clone())?;
-                            delete_textures_path[i].push(file_name.to_string());
-                        }
-                    }
-                }
-                index += 1;
-            }
-
-            for i in 1..delete_textures_path.len() {
-                let mut path = resizes_textures_path[i].clone();
-                path.push("deleted_textures.txt");
-                let mut file = File::create(path)?;
-                for file_name in &delete_textures_path[i] {
-                    file.write_all(file_name.as_bytes())?;
-                    file.write_all(b"\n")?;
-                }
-            }
-        }
-
-        Ok(())
-    }
-
-    pub fn apply_built_ressource_pack(&self, build_pack_folder: &str) -> Result<(), std::io::Error> {
-        let mut files = Vec::new();
-        let mut dirs = Vec::new();
-        let mut index = 0;
-
-        dirs.push(PathBuf::from(build_pack_folder));
-
-        while index < dirs.len() {
-            let dir = dirs[index].clone();
-            let entries = fs::read_dir(dir)?;
-            for entry in entries {
-                let entry = entry?;
-                let path = entry.path();
-                if path.is_dir() {
-                    dirs.push(path);
-                } else {
-                    files.push(path);
-                }
-            }
-            index += 1;
-        }
-
-        let total = files.len();
-        let mut done = 0;
-
-        blue!("\r{}", done);
-        cyan!("/");
-        blue!("{}", total);
-        cyan!(" files patched");
-        std::io::stdout().flush().unwrap();
-        
-        for file in files {
-            let cloned_file = file.clone();
-            let file_path = cloned_file.strip_prefix(build_pack_folder).unwrap();
-
-            let content_vec = fs::read(&cloned_file)?;
-            let content = content_vec.as_slice();
-
-            self.replace_file(format!("resources/{}", file_path.to_str().unwrap()).as_str(), content)?;
-
-            done += 1;
-
-            blue!("\r{}", done);
-            cyan!("/");
-            blue!("{}", total);
-            cyan!(" files patched");
-            std::io::stdout().flush().unwrap();
-        }
-        green_ln!("\r{} files patched                    ", total);
-        Ok(())
+        red_ln!("'{}' not found in the archive.", file_name);
+        Ok(Vec::new())
     }
 }
 
@@ -277,85 +155,31 @@ fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> std::io::Result
     Ok(())
 }
 
-pub fn build_textures(input_folder: &str, output_folder: &str) {
-    if PathBuf::from(output_folder).exists() {
-        fs::remove_dir_all(output_folder).expect("Error while removing output_folder");
-    }
-    copy_dir_all(input_folder, output_folder).expect("Error while copying input_folder to output_folder");
+pub fn compress_file(input_path: &str, output_path: &str) -> Result<(), std::io::Error> {
+    // Open the input file for reading
+    let mut input_file = File::open(input_path)?;
+    let mut buffer = Vec::new();
 
-    let resizes = vec![1, 2, 4, 8];
-    let mut resizes_textures_path = Vec::new();
-    for resize in &resizes {
-        let mut path = PathBuf::from(output_folder);
-        path.push(format!("textures/{}x", resize));
-        resizes_textures_path.push(path);
-    }
+    // Read the contents of the input file into a buffer
+    input_file.read_to_end(&mut buffer)?;
 
-    let mut index = 0;
-    for path in &resizes_textures_path {
-        if index == 0 {
-            index += 1;
-            continue;
-        }
+    // Create a new encoder and pass the input data to it
+    let mut encoder = Encoder::new(Vec::new());
+    encoder.write_all(&buffer)?;
 
-        let mut delete_textures_path = path.clone();
-        delete_textures_path.push("deleted_textures.txt");
-        if delete_textures_path.exists() {
-            let mut deleted_textures = Vec::new();
-            let file = File::open(delete_textures_path.clone()).expect("Error while opening delete_textures.txt");
-            let reader = BufReader::new(file);
-            for line in reader.lines() {
-                let line = line.expect("Error while reading delete_textures.txt");
-                deleted_textures.push(line);
-            }
+    // Finish the encoding process and retrieve the compressed data
+    let compressed = encoder.finish().into_result()?;
 
-            let mut count = 0;
+    // Create and write the compressed data into the output file
+    let mut output_file = File::create(output_path)?;
+    output_file.write_all(&compressed)?;
 
-            for deleted_texture in &deleted_textures {
-                let mut path_in = resizes_textures_path[0].clone();
-                path_in.push(deleted_texture.clone());
-                if !path_in.exists() {
-                    continue;
-                }
-                count += 1;
-            }
-
-            let mut done = 0;
-            blue!("\r{}", done);
-            cyan!("/");
-            blue!("{}", count);
-            cyan!(" textures to recreate in {}x", resizes[index]);
-            std::io::stdout().flush().unwrap();
-
-            for deleted_texture in &deleted_textures {
-                let mut path_in = resizes_textures_path[0].clone();
-                path_in.push(deleted_texture.clone());
-                if !path_in.exists() {
-                    continue;
-                }
-
-                let mut path_out = resizes_textures_path[index].clone();
-                path_out.push(deleted_texture);
-                upscale_image_pixel_perfect(path_in.to_str().unwrap(), path_out.to_str().unwrap(), resizes[index]);
-
-                done += 1;
-
-                blue!("\r{}", done);
-                cyan!("/");
-                blue!("{}", count);
-                cyan!(" textures to recreate in {}x", resizes[index]);
-                std::io::stdout().flush().unwrap();
-            }
-            green_ln!("\r{} textures recreated in {}x                 ", count, resizes[index]);
-
-            fs::remove_file(delete_textures_path).expect("Error while deleting delete_textures.txt");
-        }
-        index += 1;
-    }
+    Ok(())
 }
 
-fn upscale_image_pixel_perfect(input_path: &str, output_path: &str, upscale: u32) {
-    let img = image::open(input_path).expect("Error while opening image");
-    let img = img.resize(img.width() * upscale, img.height() * upscale, image::imageops::FilterType::Nearest);
-    img.save(output_path).expect("Error while saving image");
+pub fn decompress_bytes(input: &[u8]) -> Result<Vec<u8>, std::io::Error> {
+    let mut decoder = libflate::deflate::Decoder::new(input);
+    let mut decompressed = Vec::new();
+    decoder.read_to_end(&mut decompressed)?;
+    Ok(decompressed)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,31 @@
 use std::fs;
+use std::io::Write;
+use std::process::Command;
+use std::time::Instant;
+use downloader::Downloader;
 
 mod balamod;
+
 use clap::Parser;
-use colour::{blue, cyan, cyan_ln, green, green_ln, magenta, red_ln, yellow};
+use colour::{blue, cyan, cyan_ln, green, green_ln, magenta, magenta_ln, red_ln, yellow, yellow_ln};
 use crate::balamod::Balatro;
 
 const VERSION: &'static str = "Run this program";
+
 #[derive(Parser, Debug)]
 #[clap(version = VERSION)]
 struct Args {
-    #[clap(short='b', long="balatro-path")]
+    #[clap(short = 'x', long = "inject")]
+    inject: bool,
+    #[clap(short = 'd', long = "decompile")]
+    decompile: bool,
+    #[clap(short = 'b', long = "balatro-path")]
     balatro_path: Option<String>,
-    #[clap(short='c', long="create-pack")]
-    create_pack: bool,
-    #[clap(short='x', long="inject-pack")]
-    inject_pack: bool,
-    #[clap(long="extract-textures")]
-    extract_textures: bool,
-    #[clap(long="extract-shaders")]
-    extract_shaders: bool,
-    #[clap(long="extract-sounds")]
-    extract_sounds: bool,
-    #[clap(long="extract-fonts")]
-    extract_fonts: bool,
-    #[clap(short='a', long="extract-all")]
-    extract_all: bool,
-    #[clap(short='i', long="input")]
+    #[clap(short = 'c', long = "compress")]
+    compress: bool,
+    #[clap(short = 'i', long = "input")]
     input: Option<String>,
-    #[clap(short='o', long="output")]
+    #[clap(short = 'o', long = "output")]
     output: Option<String>,
 }
 
@@ -35,13 +33,8 @@ struct Args {
 fn main() {
     let mut args = Args::parse();
 
-    if args.create_pack && args.inject_pack {
-        red_ln!("You can't create and inject a pack at the same time!");
-        return;
-    }
-
-    if !args.create_pack && !args.inject_pack {
-        red_ln!("You must specify if you want to create or inject a pack!");
+    if args.inject && args.decompile {
+        red_ln!("You can't use -x and -d at the same time!");
         return;
     }
 
@@ -94,34 +87,204 @@ fn main() {
         }
     }
 
-    if args.extract_all {
-        args.extract_textures = true;
-        args.extract_shaders = true;
-        args.extract_sounds = true;
-        args.extract_fonts = true;
+    if args.inject {
+        if args.input.clone().is_none() {
+            args.input = Some("Balatro.lua".to_string());
+        }
+
+        if args.output.clone().is_none() {
+            args.output = Some("DAT1.jkr".to_string());
+        }
+
+        let mut need_cleanup = false;
+        let mut compress_start: Instant = Instant::now();
+        let mut compress_duration: std::time::Duration = Instant::now().duration_since(Instant::now());
+        if args.compress {
+            let mut compression_output = String::new();
+            if args.output.clone().unwrap().ends_with(".lua") {
+                compression_output = args.output.clone().unwrap().split(".lua").collect::<String>();
+            } else {
+                compression_output = args.output.clone().unwrap().clone();
+            }
+            if !compression_output.ends_with(".jkr") {
+                compression_output.push_str(".jkr");
+            }
+
+            if fs::metadata(compression_output.as_str()).is_ok() {
+                yellow_ln!("Deleting existing file...");
+                fs::remove_file(compression_output.as_str()).expect("Error while deleting file");
+            }
+
+            cyan_ln!("Compressing...");
+            compress_start = Instant::now();
+            balamod::compress_file(args.input.clone().unwrap().as_str(), compression_output.as_str()).expect("Error while compressing file");
+            compress_duration = compress_start.elapsed();
+            if !compression_output.eq_ignore_ascii_case(args.input.as_ref().unwrap()) {
+                need_cleanup = true;
+                args.input = Some(compression_output);
+            }
+            green_ln!("Done!");
+        }
+
+        let input_bytes = fs::read(args.input.clone().unwrap()).expect("Error while reading input file");
+        let input_bytes = input_bytes.as_slice();
+
+        cyan_ln!("Injecting...");
+        let inject_start = Instant::now();
+        balatro.replace_file(args.output.unwrap().as_str(), input_bytes).expect("Error while replacing file");
+        let inject_duration = inject_start.elapsed();
+        green_ln!("Done!");
+
+        if need_cleanup {
+            yellow_ln!("Cleaning up...");
+            fs::remove_file(args.input.unwrap()).expect("Error while deleting file");
+            green_ln!("Done!");
+        }
+
+        if args.compress {
+            magenta_ln!("Compression took {:?}", compress_duration);
+        }
+        magenta_ln!("Injection took {:?}", inject_duration);
     }
 
-    if args.create_pack {
-        if let Some(output) = args.output {
-            balatro.extract_ressource_pack(&output, args.extract_textures, args.extract_shaders, args.extract_sounds, args.extract_fonts).expect("Error while extracting ressources");
-            green_ln!("Done!")
-        } else {
-            red_ln!("You must specify an output directory!");
+    if args.decompile {
+        if fs::metadata("Balatro.lua").is_ok() {
+            blue!("Balatro.lua already exists, do you want to replace it? [y/N] ");
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).expect("Error while reading input");
+            let input = input.trim();
+            if input.eq_ignore_ascii_case("y") || input.eq_ignore_ascii_case("yes") {
+                yellow_ln!("Deleting existing file...");
+                fs::remove_file("Balatro.lua").expect("Error while deleting file");
+            } else {
+                yellow_ln!("Leaving...");
+                return;
+            }
+        }
+
+        cyan_ln!("Extracting...");
+        let exctract_start = Instant::now();
+        let bytes = balatro.get_file_data("DAT1.jkr").expect("Error while getting file data");
+        let exctract_duration = exctract_start.elapsed();
+        green_ln!("Done!");
+
+        cyan_ln!("Decompressing...");
+        let decompress_start = Instant::now();
+        let bytes = balamod::decompress_bytes(bytes.as_slice()).expect("Error while decompressing bytes");
+        let decompress_duration = decompress_start.elapsed();
+        let mut file = fs::File::create("DAT1.luajit").expect("Error while creating file");
+        file.write_all(bytes.as_slice()).expect("Error while writing file");
+        green_ln!("Done!");
+
+        let mut header = [0; 13];
+        header.copy_from_slice(&bytes[3..16]);
+
+        if header.iter().all(|&x| (x >= 32 && x <= 126) || (x >= 10 && x <= 13)) {
+            magenta_ln!("Balatro has already been decompiled, skipping... (if you need original sources then repair the game on Steam)");
+            fs::rename("DAT1.luajit", "Balatro.lua").expect("Error while renaming file");
+            green_ln!("Done!");
+
+            cyan_ln!("Downloading lastest deobfuscation map...");
+            let mut downloader = Downloader::builder()
+                .parallel_requests(1)
+                .build()
+                .unwrap();
+
+            let dl = downloader::Download::new(format!("https://gist.githubusercontent.com/UwUDev/a2b34bf14d5f04a04719d237549ccb88/raw/deobfmap.json?time={}", Instant::now().elapsed().as_secs().to_string().as_str()).as_str());
+            downloader.download(&[dl]).unwrap();
+            green_ln!("Done!");
+
+            cyan_ln!("Deobfuscating...");
+            let mut balatro_lua = fs::read_to_string("Balatro.lua").expect("Error while reading Balatro.lua");
+
+            let deobf_start = Instant::now();
+            balatro_lua = deobf(balatro_lua);
+            let deobf_duration = deobf_start.elapsed();
+
+            let mut file = fs::File::create("Balatro.lua").expect("Error while creating file");
+            file.write_all(balatro_lua.as_bytes()).expect("Error while writing file");
+            green_ln!("Done!");
+
+            magenta_ln!("Extraction took {:?}\nDecompression took {:?}\nDeobfuscation took {:?}", exctract_duration, decompress_duration, deobf_duration);
             return;
         }
+
+        if !fs::metadata("luajit-decompiler-v2.exe").is_ok() {
+            yellow_ln!("Downloading luajit-decompiler-v2.exe...");
+            let mut downloader = Downloader::builder()
+                .parallel_requests(1)
+                .build()
+                .unwrap();
+
+            let dl = downloader::Download::new("https://github.com/marsinator358/luajit-decompiler-v2/releases/latest/download/luajit-decompiler-v2.exe");
+            downloader.download(&[dl]).unwrap();
+            green_ln!("Done!");
+        }
+
+        let decompile_start = Instant::now();
+        if cfg!(target_os = "windows") {
+            cyan_ln!("Decompiling...");
+            Command::new("luajit-decompiler-v2.exe")
+                .arg("DAT1.luajit")
+                .output()
+                .expect("Error while executing luajit-decompiler-v2.exe");
+            green_ln!("Done!");
+        } else if cfg!(target_os = "linux") {
+            cyan_ln!("Decompiling...");
+            Command::new("wine")
+                .arg("luajit-decompiler-v2.exe")
+                .arg("DAT1.luajit")
+                .output()
+                .expect("Error while executing luajit-decompiler-v2.exe");
+            green_ln!("Done!");
+        }
+        let decompile_duration = decompile_start.elapsed();
+
+        cyan_ln!("Cleaning up...");
+        fs::rename("output/DAT1.lua", "Balatro.lua").expect("Error while renaming file");
+        fs::remove_dir_all("output").expect("Error while deleting directory");
+        fs::remove_file("DAT1.luajit").expect("Error while deleting file");
+        green_ln!("Done!");
+
+        if fs::metadata("deobfmap.json").is_ok() {
+            yellow_ln!("Deleting old deobfuscation map...");
+            fs::remove_file("deobfmap.json").expect("Error while deleting file");
+        }
+
+        cyan_ln!("Downloading lastest deobfuscation map...");
+        let mut downloader = Downloader::builder()
+            .parallel_requests(1)
+            .build()
+            .unwrap();
+
+        let dl = downloader::Download::new(format!("https://gist.githubusercontent.com/UwUDev/a2b34bf14d5f04a04719d237549ccb88/raw/deobfmap.json?time={}", Instant::now().elapsed().as_secs().to_string().as_str()).as_str());
+        downloader.download(&[dl]).unwrap();
+        green_ln!("Done!");
+
+        cyan_ln!("Deobfuscating...");
+        let mut balatro_lua = fs::read_to_string("Balatro.lua").expect("Error while reading Balatro.lua");
+
+        let deobf_start = Instant::now();
+        balatro_lua = deobf(balatro_lua);
+        let deobf_duration = deobf_start.elapsed();
+
+        let mut file = fs::File::create("Balatro.lua").expect("Error while creating file");
+        file.write_all(balatro_lua.as_bytes()).expect("Error while writing file");
+        green_ln!("Done!");
+
+        magenta_ln!("Extraction took {:?}\nDecompression took {:?}\nDecompilation took {:?}\nDeobfuscation took {:?}", exctract_duration, decompress_duration, decompile_duration, deobf_duration);
+    }
+}
+
+fn deobf(balatro_lua: String) -> String {
+    let deobfmap = fs::read_to_string("deobfmap.json").expect("Error while reading deobfuscation map");
+    let deobfmap: serde_json::Value = serde_json::from_str(deobfmap.as_str()).expect("Error while parsing deobfuscation map");
+
+    let mut balatro_lua = balatro_lua;
+
+    for (key, value) in deobfmap.as_object().unwrap() {
+        balatro_lua = balatro_lua.replace(key, value.as_str().unwrap());
     }
 
-    if args.inject_pack {
-        if let Some(input) = args.input {
-            let input_built = format!("{}_built", input);
-            balamod::build_textures(&input, &input_built);
-            balatro.apply_built_ressource_pack(&input_built).expect("Error while applying ressources");
-            cyan_ln!("Cleaning up...");
-            fs::remove_dir_all(&input_built).expect("Error while deleting rebuilt ressource pack");
-            green_ln!("Done!")
-        } else {
-            red_ln!("You must specify an input directory!");
-            return;
-        }
-    }
+    balatro_lua
 }
